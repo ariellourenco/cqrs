fe80707ea5d9a18a02dd4f541e2b9d034bfc990c
Ariel Lourenco <ariellourenco@users.noreply.github.com>
2021-04-07 16:12:52 -0300

    Make reservation expiry

--- src/Services/Registration/Registration.Domain/SeatsAvailability.cs
+++ src/Services/Registration/Registration.Domain/SeatsAvailability.cs
@@ -1,15 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Registration
 {
     public sealed class SeatsAvailability
     {
-        public void AddSeats(int v, Guid ticketTypeId)
+        private readonly Guid _id;
+
+        // Using a private collection field for better encapsulation so new reservations can
+        // not be added from "outside" of SeatsAvailability aggregate directly to the collection,
+        // but only through the method SeatsAvailability.MakeReservation() which includes behaviour.
+        private IDictionary<Guid, int> _pendingReservations;
+
+        public SeatsAvailability(Guid id)
         {
+            _id = id;
+            _pendingReservations = new Dictionary<Guid, int>();
         }
 
-        public void MakeReservation(Guid guid, int v, Guid ticketTypeId)
+        private int RemainingSeats { get; set; }
+
+        public void AddSeats(int additionalSeats)
         {
+            RemainingSeats += additionalSeats;
+        }
+
+        public void MakeReservation(Guid reservationId, int numberOfSeats)
+        {
+            if (numberOfSeats > RemainingSeats)
+                throw new ArgumentOutOfRangeException(nameof(numberOfSeats));
+
+            _pendingReservations.Add(reservationId, numberOfSeats);
+            RemainingSeats -= numberOfSeats;
+        }
+
+        public void Expires(Guid reservationId)
+        {
+            var numberOfSeats = _pendingReservations[reservationId];
+            _pendingReservations.Remove(reservationId);
+            RemainingSeats += numberOfSeats;
         }
     }
 }
--- src/Services/Registration/Registration.Tests/SeatsAvailabilityTests.cs
+++ src/Services/Registration/Registration.Tests/SeatsAvailabilityTests.cs
@@ -1,25 +1,77 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Registration
 {
     public sealed class SeatsAvailabilityTests
     {
-        private static readonly Guid TicketTypeId = Guid.NewGuid();
+        private static readonly Guid ReservationId = Guid.NewGuid();
 
         public SeatsAvailability GivenAvailableSeats()
         {
-            var sut = new SeatsAvailability();
-            sut.AddSeats(10, TicketTypeId);
+            var sut = new SeatsAvailability(ReservationId);
+            sut.AddSeats(10);
+
+            return sut;
+        }
+
+        public SeatsAvailability GivenSomeAvilableSeatsAndSomeTaken()
+        {
+            var sut = GivenAvailableSeats();
+            sut.MakeReservation(ReservationId, 6);
 
             return sut;
         }
 
         [Fact]
-        public void WhenReservingLessSeatsThanAvailableThenSucceeds()
+        public void WhenReservingLessSeatsThanTotalThenSucceeds()
         {
             var sut = GivenAvailableSeats();
-            sut.MakeReservation(Guid.NewGuid(), 4, TicketTypeId);
+            sut.MakeReservation(Guid.NewGuid(), 4);
+        }
+
+        [Fact]
+        public void WhenReservingLessSeatsThanRemainingThenSucceeds()
+        {
+            var sut = GivenSomeAvilableSeatsAndSomeTaken();
+            sut.MakeReservation(Guid.NewGuid(), 4);
+        }
+
+        [Fact]
+        public void WhenReservingMoreSeatsThanTotalThenFails()
+        {
+            var sut = GivenAvailableSeats();
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.MakeReservation(Guid.NewGuid(), 11));
+        }
+
+        [Fact]
+        public void WhenReservingMoreSeatsThanRemainingThenFails()
+        {
+            var sut = GivenSomeAvilableSeatsAndSomeTaken();
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.MakeReservation(Guid.NewGuid(), 5));
+        }
+
+        [Fact]
+        public void WhenReservationExpiresThenSucceeds()
+        {
+            var sut = GivenSomeAvilableSeatsAndSomeTaken();
+            sut.Expires(ReservationId);
+        }
+
+        [Fact]
+        public void WhenReservationExpiresThenCanReuseSeats()
+        {
+            var sut = GivenSomeAvilableSeatsAndSomeTaken();
+            sut.Expires(ReservationId);
+            sut.MakeReservation(ReservationId, 8);
+        }
+
+        [Fact]
+        public void WhenAnInexistantReservationExpiresThenFails()
+        {
+            var sut = GivenSomeAvilableSeatsAndSomeTaken();
+            Assert.Throws<KeyNotFoundException>(() => sut.Expires(Guid.NewGuid()));
         }
     }
 }
